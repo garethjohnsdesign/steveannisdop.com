b43ca5aad98751ff4b9f2bf121b2a073e8eeb5ba
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-07-15 17:28:11 +0100

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,21 @@
+import $ from "jquery";
+import Foundation from 'foundation-sites';
+import AOS from 'aos';
+import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
+
+
 $(document).ready(function() {
-  function init() {
 
   // Foundation
   // ----------
   
-  Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
-  Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
-  Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
-  Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
   
 $(document).foundation();
+
 
 // 2. Animate on Scroll
 // --------------------
@@ -27,47 +33,129 @@ window.addEventListener('load', AOS.refresh);
 });
 
 
-$('.video').lightGallery({
-    counter: false,
-    youtubePlayerParams: {
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        controls: 0
-    },
-    vimeoPlayerParams: {
-        autoplay: 0,
-        title : 0,
-        byline : 0,
-        portrait : 0,
-        color : 'FFFFFF'     
-    }
-});
 
 $('[data-curtain-menu-button]').click(function(){
   $('body').toggleClass('curtain-menu-open');
-})
+});
+
+// 2. Hover Effect
+// ---------------
 
 
+{
+    const mapNumber = (X,A,B,C,D) => (X-A)*(D-C)/(B-A)+C;
+	const getMousePos = (e) => {
+        let posx = 0;
+        let posy = 0;
+		if (!e) e = window.event;
+		if (e.pageX || e.pageY) {
+            posx = e.pageX;
+			posy = e.pageY;
+		}
+		else if (e.clientX || e.clientY) 	{
+			posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+        return { x : posx, y : posy }
+    }
+    // Generate a random float.
+    const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
 
-  }
+    /**
+     * One class per effect. 
+     * Lots of code is repeated, so that single effects can be easily used. 
+     */
 
-  // Run once
-  // ---------------------------------------------
+    // Effect 1
+    class HoverImgFx1 {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.reveal = document.createElement('div');
+            this.DOM.reveal.className = 'hover-reveal';
+            this.DOM.reveal.innerHTML = `<div class="hover-reveal__inner"><div class="hover-reveal__img" style="background-image:url(${this.DOM.el.dataset.img})"></div></div>`;
+            this.DOM.el.appendChild(this.DOM.reveal);
+            this.DOM.revealInner = this.DOM.reveal.querySelector('.hover-reveal__inner');
+            this.DOM.revealInner.style.overflow = 'hidden';
+            this.DOM.revealImg = this.DOM.revealInner.querySelector('.hover-reveal__img');
 
-  init();
+            this.initEvents();
+        }
+        initEvents() {
+            this.positionElement = (ev) => {
+                const mousePos = getMousePos(ev);
+                const docScrolls = {
+                    left : document.body.scrollLeft + document.documentElement.scrollLeft, 
+                    top : document.body.scrollTop + document.documentElement.scrollTop
+                };
+                this.DOM.reveal.style.top = `${mousePos.y+20-docScrolls.top}px`;
+                this.DOM.reveal.style.left = `${mousePos.x+20-docScrolls.left}px`;
+            };
+            this.mouseenterFn = (ev) => {
+                this.positionElement(ev);
+                this.showImage();
+            };
+            this.mousemoveFn = ev => requestAnimationFrame(() => {
+                this.positionElement(ev);
+            });
+            this.mouseleaveFn = () => {
+                this.hideImage();
+            };
+            
+            this.DOM.el.addEventListener('mouseenter', this.mouseenterFn);
+            this.DOM.el.addEventListener('mousemove', this.mousemoveFn);
+            this.DOM.el.addEventListener('mouseleave', this.mouseleaveFn);
+        }
+        showImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
 
-  // ---------------------------------------------
-  // Page Transitions
-  // ---------------------------------------------
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    this.DOM.reveal.style.opacity = 1;
+                    TweenMax.set(this.DOM.el, {zIndex: 1000});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '-100%'},
+                x: '0%'
+            }), 'begin')
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                startAt: {x: '100%'},
+                x: '0%'
+            }), 'begin');
+        }
+        hideImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
 
-  var swup = new Swup({
-    animationSelector: '[class*="swup-transition-"]',
-    elements: ['#main, #maintest']
-  });
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: 999});
+                },
+                onComplete: () => {
+                    TweenMax.set(this.DOM.el, {zIndex: ''});
+                    TweenMax.set(this.DOM.reveal, {opacity: 0});
+                }
+            })
+            .add('begin')
+            .add(new TweenMax(this.DOM.revealInner, 0.2, {
+                ease: Sine.easeOut,
+                x: '100%'
+            }), 'begin')
+            
+            .add(new TweenMax(this.DOM.revealImg, 0.2, {
+                ease: Sine.easeOut,
+                x: '-100%'
+            }), 'begin');
+        }
+    }
 
-  swup.on('contentReplaced', function () {
-    init();
-  });
+    
+    [...document.querySelectorAll('[data-fx="1"] > a, a[data-fx="1"]')].forEach(link => new HoverImgFx1(link));
+
+}
 
 });
